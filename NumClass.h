db670a33bb2ca0be9439512e01ff969368154371
haimgoldfisher <haimgoldfisher@LAPTOP-C1T4LPUA.localdomain>
2021-11-04 23:07:29 +0200

    new commit

--- NumClass.h
+++ NumClass.h
@@ -1,3 +1,6 @@
+# ifndef NUMCLASS_H
+# define NUMCLASS_H
+
 /* will return if a number is Armstrong number
 An Armstrong number is an n-digit number that is equal to the sum of the nth powers of its digits.
 For Example: 407 = 43 + 03 + 73 = 64 + 0 + 343 = 407
@@ -14,3 +17,5 @@ int isPrime(int);
 1! + 4! + 5! = 145
 */
 int isStrong(int);
+
+#endif
